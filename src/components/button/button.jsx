ee7242958df57378c02bd882dae737c745074339
PauloCPaiva <paulocesarapjr@gmail.com>
2025-03-28 14:43:26 -0300

    first commit

---
+++ src/components/button/button.jsx
@@ -0,0 +1,10 @@
+import styles from "../button/button.module.css"
+
+const Button = (props, link) => {
+    return(
+        <div className={styles.WrapperButton}>
+            <a href={link} className={styles.linkBtn} {...props}></a>
+        </div>
+    )
+}
+export default Button;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import styles from "../header/header.module.css";
+import LogoHeader from "../../assets/logo.svg";
+import iconCloser from "../../assets/menu-closer.svg";
+import iconBars from "../../assets/menu-togle.svg";
+import Button from "../button/button";
+
+const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+  return (
+    <header className={`${styles.Header} ${menuActive ? styles.active : ""}`}>
+      <div className={styles.wrapperHeader}>
+        <div>
+          <button>
+            <img src={LogoHeader} alt="" />
+          </button>
+        </div>
+        <button onClick={toggleMenu} id={styles.menuIcons}>
+          <img src={menuActive ? iconCloser : iconBars} alt="" />
+        </button>
+        <nav className={menuActive ? styles.active: ""}>
+          <ul className={styles.ulMenu}>
+            <li className={styles.navLi} >
+              <a href="">Início</a>
+            </li>
+            <li className={styles.navLi} >
+              <a href="">Carros</a>
+            </li>
+            <li className={styles.navLi} >
+              <a href="">Manutenção</a>
+            </li>
+            <li className={styles.navLi} >
+              <a href="">Novos</a>
+            </li>
+            <li className={styles.liBtn}>
+              <Button>Comprar</Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
